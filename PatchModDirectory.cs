f5e2b50d572997e118f424dbfc8a0f830a9be028
Ghosty <104216517+ghosstyy11@users.noreply.github.com>
2026-06-09 16:12:18 +0100

    Add files via upload

---
+++ PatchModDirectory.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+using HarmonyLib;
+using UnityEngine;
+using Modio.FileIO;
+
+namespace DatasaveFix.Patches
+{
+    [HarmonyPatch(typeof(BaseDataStorage), "Init")]
+    class PatchModDirectory
+    {
+        static void Postfix()
+        {
+            try
+            {
+                string newDir = Path.Combine(Application.persistentDataPath, "luauDataSaves");
+
+                typeof(Bindings.JSON).GetField("ModIODirectory",
+                    System.Reflection.BindingFlags.NonPublic |
+                    System.Reflection.BindingFlags.Static).SetValue(null, newDir);
+            }
+            catch (Exception e)
+            {
+                Debug.Log("Failed to redirect ModIODirectory: " + e);
+            }
+        }
+    }
+}
