45bbca967f5a4cd94b9fa1f6403c2746d98c5e87
Ghosty <104216517+ghosstyy11@users.noreply.github.com>
2026-06-09 16:13:27 +0100

    Add files via upload

---
+++ Plugin.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using BepInEx;
+using UnityEngine;
+using Utilla;
+using HarmonyLib;
+using System.Reflection;
+
+namespace DatasaveFix
+{
+    [BepInPlugin("com.ghosty.datasavefix", "DatasaveFix", "1.0.0")]
+    public class Plugin : BaseUnityPlugin
+    {
+        void OnEnable()
+        {
+            ApplyHarmonyPatches();
+        }
+
+        private static Harmony instance;
+
+        public static bool IsPatched { get; private set; }
+        public const string InstanceId = "com.ghosty.datasavefix";
+
+        internal static void ApplyHarmonyPatches()
+        {
+            if (!IsPatched)
+            {
+                if (instance == null)
+                {
+                    instance = new Harmony(InstanceId);
+                }
+
+                instance.PatchAll(Assembly.GetExecutingAssembly());
+                IsPatched = true;
+            }
+        }
+    }
+}
